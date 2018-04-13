f9c8164dded9c52b181ca344b24c3e891505ed0e
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2018-04-13 14:09:08 +0200

    added comment about handlers in app.js

--- request_axios_promises/app.js
+++ request_axios_promises/app.js
@@ -1,5 +1,12 @@
 const yargs = require("yargs");
 
+/*
+*
+* ALL FOUR HANDLERS GIVES THE SAME RESULT,
+* ONLY DIFFRENCE IS THER WAY TO HANDLE DATA
+* (LIKE CALLBACKS/PROMISES/ASYNC_AND_AWAIT)
+*
+*/
 // const handler = require("./request_callback/handler");
 // const handler = require("./request_promises/handler");
 // const handler = require("./axios_promises/handler");
